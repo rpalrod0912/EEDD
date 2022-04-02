ff016d24dd892661f514eea0b39e520560d564fc
rpalrod0912 <rafapr0001@gmail.com>
2022-04-02 11:09:15 +0200

    Peste añadida al repositorio

---
+++ Escritorio/JuegoMedieval/Peste.py
@@ -0,0 +1,18 @@
+from Ciudadanos import Ciudadanos
+
+class Peste(Ciudadanos):
+    """Class Peste
+    """
+    # Attributes:
+    
+    # Operations
+    def PerdidaPoblacion(self, ):
+        """function PerdidaPoblacion
+        
+        : 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+
