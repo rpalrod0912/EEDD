49a0f629126dbf8b07a6ab5d4c38496c2ae94ab8
rpalrod0912 <rafapr0001@gmail.com>
2022-04-02 11:22:19 +0200

    Añadidos Militares.py y Recaudar.py

---
+++ Escritorio/JuegoMedieval/Militares.py
@@ -0,0 +1,19 @@
+from Ciudadanos import Ciudadanos
+from Espadas y Escudos import Espadas y Escudos
+
+class Militares:
+    """Class Militares
+    """
+    # Attributes:
+    
+    # Operations
+    def Utiliza(self, Espadas, Escudos):
+        """function Utiliza
+        
+        Espadas, Escudos: 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+
---
+++ Escritorio/JuegoMedieval/Recaudar.py
@@ -0,0 +1,28 @@
+from Ciudadanos import Ciudadanos
+
+class Recaudar(Ciudadanos):
+    """Class Recaudar
+    """
+    # Attributes:
+    Felicidad = None  # () 
+    
+    # Operations
+    def MontarTaberna(self, Felicidad):
+        """function MontarTaberna
+        
+        Felicidad: 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+    def MontarEspectaculo(self, Felicidad):
+        """function MontarEspectaculo
+        
+        Felicidad: 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+
