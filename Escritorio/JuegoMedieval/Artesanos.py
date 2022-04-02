f133fc434fa9725890d678d7f5d3961adecb001f
rpalrod0912 <rafapr0001@gmail.com>
2022-04-02 10:56:53 +0200

    Añadidos Artesano.py y AtaqueBarbaro

---
+++ Escritorio/JuegoMedieval/Artesanos.py
@@ -0,0 +1,20 @@
+from Ciudadanos import Ciudadanos
+from Espadas y Escudos import Espadas y Escudos
+
+class Artesanos:
+    """Class Artesanos
+    """
+    # Attributes:
+    
+    # Operations
+    def Genera(self, Espadas, Escudos):
+        """function Genera
+        
+        Espadas: 
+        Escudos: 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+
---
+++ Escritorio/JuegoMedieval/AtaqueBarbaro.py
@@ -0,0 +1,21 @@
+from Militares import Militares
+from Ciudadanos import Ciudadanos
+
+class AtaqueBarbaro(Ciudadanos, Militares):
+    """Class AtaqueBarbaro
+    """
+    # Attributes:
+    FuerzaDeAtaque = None  # () 
+    
+    # Operations
+    def AtacarPoblacion(self, Ciudadanos, Militares):
+        """function AtacarPoblacion
+        
+        Ciudadanos: 
+        Militares: 
+        
+        returns 
+        """
+        return None # should raise NotImplementedError()
+    
+
